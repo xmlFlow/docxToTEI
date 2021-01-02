c66987f26e4094cc0e2c8300a400c282de3390fa
withanaged <dulip.withanage@tib.eu>
2021-01-02 14:26:06 +0100

    update

--- src/docx2tei/tei/Facsimiles.php
+++ src/docx2tei/tei/Facsimiles.php
@@ -2,7 +2,6 @@
 
 namespace docx2tei\tei;
 
-use docx2tei\XMLUtils;
 use DOMDocument;
 use DOMElement;
 
@@ -17,65 +16,55 @@ class Facsimiles extends DOMDocument {
 
     function setFacsimiles(): void {
         $facsimiles = $this->document->xpath->query('//root/text/sec/title[text()="' . $this->document->cfg->sections->facsimiles . '"]/parent::sec/sec/title');
+        $abs = $this->document->xpath->query('//title[starts-with(text(),"' . $this->document->cfg->sections->edition . '")]/parent::sec/sec/title');
 
-        $sbXpathStr = '//title[starts-with(text(),"' . $this->document->cfg->sections->edition . '")]/parent::sec/sec';
-        $abs = $this->document->xpath->query($sbXpathStr);
 
-        if (count($facsimiles) == 0) {
-            XMLUtils::print_error("[Error] No facsimiles " . $this->document->cfg->sections->facsimiles);
-        } else {
-            foreach ($facsimiles as $facsimile) {
-                if ($facsimile->tagName !== "title") {
-                    XMLUtils::print_error("[Error] facsimile not formatted properly. Use  Heading 2 format in Word " . $facsimile->tagName);
+        $lastPB = null;
+        foreach ($facsimiles as $facsimile) {
+
+            list($xml_id, $facs, $fpage) = explode(":", $facsimile->textContent);
+            $surface = $this->createElement("surface");
+            $idAttrib = $this->createAttribute('xml:id');
+            $idAttrib->value = $xml_id;
+            $surface->appendChild($idAttrib);
+            $facsAttrib = $this->createAttribute('facs');
+            $facsAttrib->value = $facs;
+            $surface->appendChild($facsAttrib);
+            $this->createCoordinates($surface);
+            $pageFound = false;
+
+            foreach ($abs as $ab) {
+                list($abOrPb, $attrib, $page) = explode('@', $nodeValue = $ab->textContent);
+                if ($pageFound==false && $abOrPb == "pb" && $xml_id == ltrim($attrib, '#') && $fpage == $page) {
+                    $pageFound = true;
                 }
-                $text = (string)$facsimile->textContent;
-                if (strlen($text) == 0) {
-                    XMLUtils::print_error("[Error]  Content of facsimile is not defined or Formatting error");
-                } else {
-                    $surfaceParts = explode(":", $text);
-                    if (count($surfaceParts) < 3) {
-                        XMLUtils::print_error("[Error]  Surface formatting error. Should be e.g. in surface1: E_12.png:1r " . $surfaceParts);
-                    } else {
-                        list($xml_id, $facs, $page) = $surfaceParts;
-                        $surface = $this->createElement("surface");
-                        $idAttrib = $this->createAttribute('xml:id');
-                        $idAttrib->value = $xml_id;
-                        $surface->appendChild($idAttrib);
-                        $facsAttrib = $this->createAttribute('facs');
-                        $facsAttrib->value = $facs;
-                        $surface->appendChild($facsAttrib);
-                        $this->createCoordinates($surface);
 
-                        foreach ($abs as $ab) {
-                            $abElement = $this->document->xpath->query("title", $ab);
-                            if (count($abElement) > 0) {
-                                $nodeValue = $abElement->item(0)->nodeValue;
-                                if (substr($nodeValue, 0, 2) === "ab") {
-                                    $parts = explode('@', $nodeValue);
-                                    if (count($parts) > 1) {
-                                        $zone = $this->createElement("zone");
-                                        $idAttrib = $this->createAttribute('xml:id');
-                                        $idAttrib->value = str_replace("#", "", $parts[1]);
-                                        $zone->appendChild($idAttrib);
-                                        $this->createCoordinates($zone);
-                                        $surface->appendChild($zone);
-                                    }
-                                }
-
-                            }
-
-                        }
-                        $this->document->facsimile->appendChild($this->document->importNode($surface, true));
-                    }
+                if($pageFound && $abOrPb == "ab") {
+                    $zone = $this->createElement("zone");
+                    $idAttrib = $this->createAttribute('xml:id');
+                    $idAttrib->value = ltrim($attrib,'#');
+                    $zone->appendChild($idAttrib);
+                    $this->createCoordinates($zone);
+                    $surface->appendChild($zone);
                 }
+
+                if ($pageFound==true && $abOrPb == "pb" && $xml_id != ltrim($attrib, '#') && $fpage != $page) {
+                    $pageFound = false;
+                }
+
             }
+            $this->document->facsimile->appendChild($this->document->importNode($surface, true));
+
         }
+
+
+
     }
 
     /**
      * @param DOMElement $surface
      */
-    private function createCoordinates(DOMElement $surface): void {
+    function createCoordinates(DOMElement $surface): void {
         foreach (["ulx", "uly", "lrx", "lry"] as $attr) {
             $coord = $this->createAttribute($attr);
             $coord->value = 0;
