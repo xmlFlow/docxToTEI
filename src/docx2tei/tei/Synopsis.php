2e53cb0d981448d23ede1f0d6ad2d9e8187af5ba
withanaged <dulip.withanage@tib.eu>
2021-01-24 17:32:33 +0100

    Sysnopsis only available

--- src/docx2tei/tei/Synopsis.php
+++ src/docx2tei/tei/Synopsis.php
@@ -16,28 +16,29 @@ class Synopsis extends DOMDocument {
 
     protected function setSynopsis(): void {
         $etSec = $this->document->xpath->query('//root/text/sec/title[text()="' . $this->document->cfg->sections->synopsis . '"]/parent::sec/child::node()');
-
-        $div = $this->createElement("div");
-        $idAttrib = $this->createAttribute('xml:id');
-        $idAttrib->value = "synopsis";
-        $div->appendChild($idAttrib);
-        $typeAttr = $this->createAttribute('type');
-        $typeAttr->value = "synopsis";
-        $div->appendChild($typeAttr);
-        $langAttr = $this->createAttribute('xml:lang');
-        $langAttr->value = "eng";
-        $div->appendChild($langAttr);
-        foreach ($etSec as $et) {
-            if (strlen($et->textContent) > 0) {
-                $s = $et->ownerDocument->saveXML($et);
-                $s = XMLUtils::getMarkups($s);
-                # create spaces
-                $ab = $this->createDocumentFragment();
-                $ab->appendXML($s);
-                $div->appendChild($ab);
+        if ($etSec->length > 0) {
+            $div = $this->createElement("div");
+            $idAttrib = $this->createAttribute('xml:id');
+            $idAttrib->value = "synopsis";
+            $div->appendChild($idAttrib);
+            $typeAttr = $this->createAttribute('type');
+            $typeAttr->value = "synopsis";
+            $div->appendChild($typeAttr);
+            $langAttr = $this->createAttribute('xml:lang');
+            $langAttr->value = "eng";
+            $div->appendChild($langAttr);
+            foreach ($etSec as $et) {
+                if (strlen($et->textContent) > 0) {
+                    $s = $et->ownerDocument->saveXML($et);
+                    $s = XMLUtils::getMarkups($s);
+                    # create spaces
+                    $ab = $this->createDocumentFragment();
+                    $ab->appendXML($s);
+                    $div->appendChild($ab);
+                }
             }
-        }
             $this->document->body->appendChild($this->document->importNode($div, true));
 
+        }
     }
 }
